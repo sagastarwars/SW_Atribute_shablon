46200ab13683b5adf2ebaf0179f03b6bc6a93a28
AVazianskii <avazianskii@mail.ru>
2022-02-16 21:59:23 +0300

    renamed namespace

--- SW_Atribute_shablon/Atribute_class.cs
+++ SW_Atribute_shablon/Atribute_class.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SW_Character_Class;
-
-namespace SW_Character_Class
+﻿namespace Attribute_libs
 {
     public class Atribute_class
     {
